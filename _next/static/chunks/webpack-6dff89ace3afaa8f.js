38e47a62aad196a127106f6e42d81a9f52005996
ΛЯΛPZZ.ΣƬΉ <tokenla15@gmail.com>
2022-12-04 18:00:18 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-6dff89ace3afaa8f.js
+++ _next/static/chunks/webpack-6dff89ace3afaa8f.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "999d76be3a362591" + ".css";
+/******/ 			return "static/css/" + "449435b7e4fa2dc6" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
